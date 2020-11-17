a7be0cd5f65a36a407ebc5b8d0ef9f747d0641d9
SuduWudu69 <beans.inc.management@gmail.com>
2020-11-17 12:52:04 -0800

    ifelseifelse

---
+++ ifelse.py
@@ -0,0 +1,5 @@
+print("Hello and welcome to the Sudu Industries online job interview. I'm going to ask you a few basic questions")
+name = input("What is your first name?")
+if len(name)>10:
+    print("That's quite a long name you have" + name)
+else:
