e37833140ae2c65b261dad158b2ad98e4516a396
SuduWudu69 <sirvolker757@gmail.com>
2021-03-11 00:41:49 -0800

    garfl.py

--- garfl.py
+++ garfl.py
@@ -1,3 +1,6 @@
 garfl = True
 if garfl == True:
     print("true")
+    garfl = False
+if not garfl == True:
+    print(garfl)
--- hannah.py
+++ hannah.py
@@ -2,8 +2,9 @@ import random, time
 name_list = ['hamster', 'cat', 'tiger']
 name_chosen = name_list[random.randint(0, len(name_list)-1)]
 name_hints = []
+player_points = int(0)
 if name_chosen == 'hamster':
-    name_hints = ["I am tiny", ""]
+    name_hints = ["I am tiny", "I hoard food", ""]
 if name_chosen == 'cat':
     name_hints = []
 if name_chosen == 'tiger':
@@ -12,4 +13,4 @@ print("Hello and welcome to Hannah's game of fun.")
 time.sleep(.75)
 input("press enter to continue")
 player_name = input("To start the game off, I'm gonna need your name: ")
-print("Ok " + player_name + ", it's time to play,\n HANNAH'S GAME OF FUN!")
+print("Ok " + player_name + ", it's time to play,\nHANNAH'S GAME OF FUN!")
